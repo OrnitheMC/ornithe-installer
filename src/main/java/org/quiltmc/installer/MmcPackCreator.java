703ab47d4c9a9053e58c5700462caac6e0399451
Space Walker <spacedoesrs@gmail.com>
2023-09-13 22:23:14 +0200

    update resource io in MmcPackCreator

--- src/main/java/org/quiltmc/installer/MmcPackCreator.java
+++ src/main/java/org/quiltmc/installer/MmcPackCreator.java
@@ -17,7 +17,7 @@
 package org.quiltmc.installer;
 
 import java.io.*;
-import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -35,88 +35,65 @@ public class MmcPackCreator {
 	}
 
 	public static void compileMmcZip(File outPutDir,String gameVersion, LoaderType type, String loaderVersion, String lwjglVersion){
-		File examplePackJson = new File("src/main/resources/packformat/mmc-pack.json");
-		File exampleIntermediaryJson = new File("src/main/resources/packformat/patches/net.fabricmc.intermediary.json");
-		File exampleInstanceCfg = new File("src/main/resources/packformat/instance.cfg");
-		File icon = new File("src/main/resources/packformat/ornithe.png");
+		String examplePackDir = "packformat";
+		String packJsonPath = "mmc-pack.json";
+		String intermediaryJsonPath = "patches/net.fabricmc.intermediary.json";
+		String instanceCfgPath = "instance.cfg";
+		String iconPath = "ornithe.png";
 
 		try {
 			String transformedPackJson = transformPackJson(
-					Files.readString(examplePackJson.toPath()), gameVersion, type, loaderVersion, lwjglVersion
+					readResource(examplePackDir, packJsonPath), gameVersion, type, loaderVersion, lwjglVersion
 			);
-			String transformedIntermediaryJson = Files.readString(exampleIntermediaryJson.toPath())
+			String transformedIntermediaryJson = readResource(examplePackDir, intermediaryJsonPath)
 					.replaceAll("\\$\\{mc_version}", gameVersion);
-			String transformedInstanceCfg = Files.readString(exampleInstanceCfg.toPath())
+			String transformedInstanceCfg = readResource(examplePackDir, instanceCfgPath)
 					.replaceAll("\\$\\{mc_version}", gameVersion);
-
-			File tempDir = new File(outPutDir, ".temp");
-			if (!tempDir.exists()) {
-				tempDir.mkdir();
-			}
-
-			File packJson = new File(tempDir, "mmc-pack.json");
-			packJson.createNewFile();
-
-			File instanceCfg = new File(tempDir,"instance.cfg");
-			instanceCfg.createNewFile();
-
-			File patchesDir = new File(tempDir,"patches");
-			if (!patchesDir.exists()) {
-				patchesDir.mkdir();
-			}
-
-			File intermediaryJson = new File(patchesDir, "net.fabricmc.intermediary.json");
-			intermediaryJson.createNewFile();
-
-			writeToFile(packJson, transformedPackJson);
-			writeToFile(intermediaryJson, transformedIntermediaryJson);
-			writeToFile(instanceCfg, transformedInstanceCfg);
 
 			File zipFile = new File(outPutDir,"Ornithe-" + gameVersion + ".zip");
-			if (!zipFile.exists()) {
-				zipFile.createNewFile();
+			if (zipFile.exists()) {
+				zipFile.delete();
 			}
 
 			FileOutputStream fileOut = new FileOutputStream(zipFile);
 			ZipOutputStream zipOut = new ZipOutputStream(fileOut);
 
-			addFileToZip(zipOut, icon, icon.getName());
-			addFileToZip(zipOut, instanceCfg, instanceCfg.getName());
-			addFileToZip(zipOut, packJson, packJson.getName());
-
-			zipOut.putNextEntry(new ZipEntry(patchesDir.getName() + "/"));
-			zipOut.closeEntry();
-
-			addFileToZip(zipOut, intermediaryJson, patchesDir.getName() + "/" + intermediaryJson.getName());
+			copyResourceToZip(zipOut, examplePackDir, iconPath);
+			writeJsonToZip(zipOut, instanceCfgPath, transformedInstanceCfg);
+			writeJsonToZip(zipOut, intermediaryJsonPath, transformedIntermediaryJson);
+			writeJsonToZip(zipOut, packJsonPath, transformedPackJson);
 
 			zipOut.close();
 			fileOut.close();
-
-			intermediaryJson.delete();
-			patchesDir.delete();
-			packJson.delete();
-			instanceCfg.delete();
-			tempDir.delete();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	public static void writeToFile(File fileToBeWritten, String writing) throws IOException {
-		FileWriter fileWriter = new FileWriter(fileToBeWritten);
-		fileWriter.write(writing);
-		fileWriter.close();
+	private static String readResource(String dir, String path) throws IOException {
+		InputStream resource = MmcPackCreator.class.getResourceAsStream(String.format("%s/%s", dir, path));
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		for (int length; (length = resource.read(buffer)) != -1; ) {
+			os.write(buffer, 0, length);
+		}
+		return os.toString(StandardCharsets.UTF_8);
 	}
 
-   public static void addFileToZip(ZipOutputStream zipOut, File fileToZip, String fileName) throws IOException {
-	   FileInputStream fis = new FileInputStream(fileToZip);
-	   ZipEntry zipEntry = new ZipEntry(fileName);
-	   zipOut.putNextEntry(zipEntry);
-	   byte[] bytes = new byte[1024];
-	   int length;
-	   while ((length = fis.read(bytes)) >= 0) {
-		   zipOut.write(bytes, 0, length);
-	   }
-	   fis.close();
-   }
+	private static void copyResourceToZip(ZipOutputStream zipOut, String dir, String path) throws IOException {
+		InputStream resource = MmcPackCreator.class.getResourceAsStream(String.format("%s/%s", dir, path));
+		byte[] buffer = new byte[1024];
+		ZipEntry zipEntry = new ZipEntry(path);
+		zipOut.putNextEntry(zipEntry);
+		for (int length; (length = resource.read(buffer)) != -1; ) {
+			zipOut.write(buffer, 0, length);
+		}
+	}
+
+	private static void writeJsonToZip(ZipOutputStream zipOut, String path, String json) throws IOException {
+		byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
+		ZipEntry zipEntry = new ZipEntry(path);
+		zipOut.putNextEntry(zipEntry);
+		zipOut.write(bytes);
+	}
 }
