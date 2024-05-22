a6a31602185cee24208faeb24eb7439d0b0147ea
LostLuma <lilly@lostluma.net>
2024-05-22 20:13:51 +0200

    Change Java 8 crash message branding and links

--- src/java8/java/org/quiltmc/installer/Main.java
+++ src/java8/java/org/quiltmc/installer/Main.java
@@ -50,10 +50,10 @@ public class Main {
                 }
 
 
-                showPopup("Quilt Installer crashed!", "<html>Quilt Installer needs Java 17 to run.<br><br>" +
+                showPopup("Ornithe Installer crashed!", "<html>Ornithe Installer needs Java 17 to run.<br><br>" +
                         "Install the latest LTS release of Java from <a href=\"https://adoptium.net/\">Eclipse Adoptium</a> and try again." +
-                        "<br><br>If you need help, ask on the <a href=\"https://forum.quiltmc.org/c/9/\">Quilt Forum</a>" +
-                        " or in the <a href=\"discord.quiltmc.org\">Quilt Discord server</a>.</html>", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
+                        "<br><br>If you need help, ask" +
+                        " in the <a href=\"https://discord.gg/JbRbRf62pn\">Ornithe Discord server</a>.</html>", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
                 System.exit(1);
             }
         } catch (Throwable t) {
